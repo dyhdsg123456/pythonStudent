48bd0811921ba0e1674c2d48206cab70d047c86e
duyuanhao <duyuanhao@yicai.com>
2017-05-31 11:20:42 +0800

    python入门

--- aa.py
+++ aa.py
@@ -1,3 +1,106 @@
 # print('aaa')
-name=input('please enter your name: ');
-print('hello,',name);
+# name=input('please enter your name: ');
+# print('hello,',name);
+# print("1024*768=:",1024*768)
+# a=100
+# if a>=0:
+#     print(a)
+# else:
+#     print(-a)
+# print('I\'m ok')
+# print('I\m learning\npython')
+# a = 123 # a是整数
+# print(a)
+# a = 'ABC' # a变为字符串
+# print(a)
+# print(10/3)
+# print(10//3)
+# print( 'ABC'.encode('ascii'))
+# print( '中文'.encode('utf-8'))
+# print(len('abc'))
+# print(len('中文'.encode('utf-8')))
+# print( 'Hi, %s, you have $%d.' % ('Michael', 1000000))
+# s1=72
+# s2=85
+# r=(85-72)/72*100
+# print('%.1f %%' % r)
+# classmates = ['Michael', 'Bob', 'Tracy']
+# # print(classmates)
+# # print(len(classmates[0]))
+# # print(classmates[2])
+# classmates.insert(1,'jack');
+# print(classmates)
+# classmates.pop()
+# print(classmates)
+# L = ['Apple', 123, True]#集合可以存放不同类型元素
+# # print(L)
+# s = ['python', 'java', ['asp', 'php'], 'scheme']
+# print(len(s))
+# t = (1, 2)#不可变
+# print(t)
+# print(len(t))
+# t=(1)
+# print(t)
+# t=(1,)
+# print(t)
+# L = [
+#     ['Apple', 'Google', 'Microsoft'],
+#     ['Java', 'Python', 'Ruby', 'PHP'],
+#     ['Adam', 'Bart', 'Lisa']
+# ]
+# print(L[0][0])
+# print(L[1][1])
+# age=3
+# if age>=19:
+#     print('1')
+# elif age>=6:
+#     print('2')
+# else:
+#     print('3')
+#     x=None
+# if x:
+# #     print("True")
+# birth=int(input('birth: '))
+# if birth<2000:
+#     print('00前')
+# else:
+#     print('00后')
+# names = ['Michael', 'Bob', 'Tracy']
+# for name in names:
+#     print(name)
+# k=list(range(5))
+# print(k)
+#
+# sum=0
+# for x in range(101):
+#     sum=sum+x;
+# print(sum)
+# sum=0
+# n=99
+# while n>0:
+#     sum=sum+n
+#     n=n-2
+#
+# print(sum)
+
+# L = ['Bart', 'Lisa', 'Adam']
+# for x in L:
+#     print('hello',x)
+
+# n = 1
+# while n <= 100:
+#     if n > 10: # 当n = 11时，条件满足，执行break语句
+#         break # break语句会结束当前循环
+#     print(n)
+#     n = n + 1
+# print('END')
+# d = {'Michael': 95, 'Bob': 75, 'Tracy': 85}
+# print(d['Michael'])
+# print(d.get('Michael1'))
+# s = set([1, 1, 2, 2, 3, 3])
+# print(s)
+# n1 = 255
+# n2 = 1000
+# print(hex(n1), hex(n2))
+
+
