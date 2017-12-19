136991dd916ea423cccb5953860befd92f642402
duyuanhao <13775516233@163.com>
2017-12-19 16:03:17 +0800

    python入门

---
+++ 异步IO/async/await.py
@@ -0,0 +1,33 @@
+# import asyncio
+#
+# @asyncio.coroutine
+# def hello():
+#     print("Hello world!")
+#     # 异步调用asyncio.sleep(1):
+#     r = yield from asyncio.sleep(1)
+#     print("Hello again!")
+#
+# # 获取EventLoop:
+# loop = asyncio.get_event_loop()
+# # 执行coroutine
+# loop.run_until_complete(hello())
+# loop.close()
+
+import threading
+import asyncio
+
+# @asyncio.coroutine
+# def hello():
+#     print('Hello world! (%s)' % threading.currentThread())
+#     yield from asyncio.sleep(1)
+#     print('Hello again! (%s)' % threading.currentThread())
+async def hello():
+    print("Hello world!")
+    r = await asyncio.sleep(1)
+    print("Hello again!")
+
+
+loop = asyncio.get_event_loop()
+tasks = [hello(), hello()]
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
